d8965e295f43debf8b506b2d012e2872086a030c
Harshvardhan Sahay <bhvima@gmail.com>
2022-01-22 10:23:19 -0500

    Read gyroscope and accelerometer data

---
+++ test/test.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/* Set the delay between fresh samples */
+#define BNO055_SAMPLERATE_DELAY_MS (10)
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+void setup(void) {
+  Serial.begin(115200);
+
+  /* 
+  Initialize the sensor in IMU mode
+  - In the IMU mode the relative orientation of the BNO055
+    in space is calculated from the accelerometer and 
+    gyroscope data.
+  */
+  if(!bno.begin(Adafruit_BNO055::OPERATION_MODE_IMUPLUS)) {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  delay(1000);
+
+  /* Display the current temperature */
+  int8_t temp = bno.getTemp();
+  Serial.print("Current Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.println("");
+
+  bno.setExtCrystalUse(true);
+}
+
+void loop(void) {
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  Serial.print("X: ");
+  Serial.print(euler.x());
+  Serial.print(" Y: ");
+  Serial.print(euler.y());
+  Serial.print(" Z: ");
+  Serial.print(euler.z());
+  Serial.print("\n");
+
+  imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  Serial.print("X: ");
+  Serial.print(accel.x());
+  Serial.print(" Y: ");
+  Serial.print(accel.y());
+  Serial.print(" Z: ");
+  Serial.print(accel.z());
+  Serial.print("\n");
+}
