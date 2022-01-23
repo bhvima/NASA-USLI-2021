bd30b8c84999bc82ac92418bacc3a09f82c49c13
Harshvardhan Sahay <bhvima@gmail.com>
2022-01-23 06:44:59 -0500

    Added images

--- test/test.ino
+++ test/test.ino
@@ -35,7 +35,41 @@ void setup(void) {
   bno.setExtCrystalUse(true);
 }
 
+double xPos = 0, yPos = 0, headingVel = 0;
+double ACCEL_VEL_TRANSITION =  (double)(BNO055_SAMPLERATE_DELAY_MS) / 1000.0;
+double ACCEL_POS_TRANSITION = 0.5 * ACCEL_VEL_TRANSITION * ACCEL_VEL_TRANSITION;
+double DEG_2_RAD = 0.01745329251;
+
 void loop(void) {
+
+  unsigned long tStart = micros();
+
+  sensors_event_t orientationData , linearAccelData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+  bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+  xPos = xPos + ACCEL_POS_TRANSITION * linearAccelData.acceleration.x;
+  yPos = yPos + ACCEL_POS_TRANSITION * linearAccelData.acceleration.y;
+
+  // velocity of sensor in the direction it's facing
+  headingVel = ACCEL_VEL_TRANSITION * linearAccelData.acceleration.x / cos(DEG_2_RAD * orientationData.orientation.x);
+
+  Serial.print("Heading: ");
+  Serial.println(orientationData.orientation.x);
+  Serial.print("Position: ");
+  Serial.print(xPos);
+  Serial.print(" , ");
+  Serial.println(yPos);
+  Serial.print("Speed: ");
+  Serial.println(headingVel);
+  Serial.println("-------");
+
+  while ((micros() - tStart) < (BNO055_SAMPLERATE_DELAY_MS * 1000))
+  {
+    //poll until the next sample is ready
+  }
+
+  /*
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
   Serial.print("X: ");
   Serial.print(euler.x());
@@ -53,4 +87,5 @@ void loop(void) {
   Serial.print(" Z: ");
   Serial.print(accel.z());
   Serial.print("\n");
+  */
 }
